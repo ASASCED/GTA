ba6ff6e6b64499239f362f8b3168f3c00f47d29d
Jesus Jimenez Cordero <jesus_heaven117@outlook.com>
2019-04-14 03:49:07 -0500

    Home Page, test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  produc: any[] = [
+    {
+      nombre: 'Arina',
+      img: '/assets/img1.jpg'
+    },
+    {
+      nombre: 'Arroz',
+      img: '/assets/img2.jpg'
+    },
+    {
+      nombre: 'Leche',
+      img: '/assets/img3.jpg'
+    },
+    {
+      nombre: 'Huevos',
+      img: '/assets/img4.jpg'
+    },
+    {
+      nombre: 'Refresco',
+      img: '/assets/img5.jpg'
+    },
+    {
+      nombre: 'Sopa',
+      img: '/assets/img6.jpg'
+    },
+    {
+      nombre: 'Queso',
+      img: '/assets/img7.jpg'
+    }
+  ];
+
 }
